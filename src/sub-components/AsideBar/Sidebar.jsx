4a6431744881bc5aa91cf181a984cac13c07a929
Afzal14786 <mdafzal14777@gmail.com>
2025-07-26 00:15:09 +0530

    create /images for assets

---
+++ src/sub-components/AsideBar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    // sidebar will be static in the left side of the dashboard
+    <>
+        <aside className='fixed top-10 left-0 h-screen w-123 text-white p-4'>
+
+        </aside>
+    </>
+  )
+}
+
+export default Sidebar
