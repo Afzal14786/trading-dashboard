dd871dd953db8ef0c2515ca37aac9a2ada4bfadb
Afzal14786 <mdafzal14777@gmail.com>
2025-08-14 01:14:33 +0530

    implementing sensex & nifty headbar

--- src/Components/NeftyFiftyBar.jsx
+++ src/Components/NeftyFiftyBar.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
 import DashboardNavbar from "./DashboardNavbar"
 
 const NeftyFiftyBar = () => {
   return (
     <>
       <div className='top-bar-container'>
-          <div className=''>
-            
-          </div>
+          
+          <nav className="nifty-container">
+              <div className="nifty">
+                <span className="index">NIFTY 50</span>
+                <span className="price">{24619.35}</span>
+                <span className="price">131.95</span>
+                <span className="percent">{(0.54)} </span>
+              </div>
+
+              <div className="sensex">
+                <span className="index">SENSEX</span>
+                <span className="price">{80539.91}</span>
+                <span className="price">24619</span>
+                <span className="percent">{0.51}</span>
+              </div>
+          </nav>
 
           {/* dashboard navmar */}
           <DashboardNavbar/>
