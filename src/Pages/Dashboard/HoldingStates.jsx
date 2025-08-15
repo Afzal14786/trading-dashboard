81eab913e01c6cba3c38f397f2949b06b1d06d98
Afzal14786 <mdafzal14777@gmail.com>
2025-08-16 01:23:49 +0530

    Add New Image

---
+++ src/Pages/Dashboard/HoldingStates.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./style.css"
+
+
+const HoldingStates = () => {
+  return (
+    <>
+        <div className="holding_portfolio">
+            <div className='HoldingS__info common'>
+                <div className="empty__state">
+                    {/* there should be an image  */}
+                    <img src="/images/holdings__bag__images.svg" alt="holdings__bag__images" className='holding_image'/>
+                    <div className="holding__description">
+                        <p>You don't have any stocks in your DEMAT yet. Get started with absolutely free equity investments.</p>
+                    </div>
+                    <button className='investing__btn'>Start Investing</button>
+                </div>
+            </div>
+
+            <div className="empty__div common"></div>
+        </div>
+    </>
+  );
+}
+
+export default HoldingStates
