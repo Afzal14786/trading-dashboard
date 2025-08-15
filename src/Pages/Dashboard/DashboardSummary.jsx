181b69adbe76c1db725599a3db1cab059f312ffd
Afzal14786 <mdafzal14777@gmail.com>
2025-08-15 23:45:26 +0530

    implementing dashboard equity and commodity part

--- src/Pages/Dashboard/DashboardSummary.jsx
+++ src/Pages/Dashboard/DashboardSummary.jsx
@@ -1,11 +1,66 @@
 import React from 'react'
 import "./style.css"
+import PieChartOutlineIcon from '@mui/icons-material/PieChartOutline';
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
 
 const DashboardSummary = () => {
   return (
     <>
       <div className='summaryPage summary'>
-          <h1>Hello</h1>
+          <div className="user__name">
+              Hi, Md Afzal Ansari
+          </div>
+
+        {/* equity and commodity information cards */}
+        <div className="cards__container">
+            {/* Equity Card */}
+            <div className="card">
+                <div className="card__header">
+                  <PieChartOutlineIcon className='chart__icon'/> Equity
+                </div>
+                <div className="card__body">
+                  <div className="card__left">
+                    <div className='title'>0</div>
+                    <div className="lable">Margin available</div>
+                  </div>
+                  <div className="card__right">
+                    <div className="block">
+                      <span>Margin Used</span>
+                      <span>0</span>
+                    </div>
+                    <div className="block">
+                      <span>Opening balance</span>
+                      <span>0</span>
+                    </div>
+                    <a href="#" className='view__statement'>View Statement</a>
+                  </div>
+                </div>
+            </div>
+
+            {/* Commodity Card */}
+            <div className="card">
+                <div className="card__header">
+                  <WaterDropIcon className='chart__icon'/> Commodity
+                </div>
+                <div className="card__body">
+                  <div className="card__left">
+                      <div className='title'>0</div>
+                      <div className="lable">Margin available</div>
+                  </div>
+                  <div className="card__right">
+                    <div className="block">
+                      <span>Margin Used</span>
+                      <span>0</span>
+                    </div>
+                    <div className="block">
+                      <span>Opening balance</span>
+                      <span>0</span>
+                    </div>
+                    <a href="#" className='view__statement'>View Statement</a>
+                  </div>
+                </div>
+            </div>
+        </div>
       </div>
     </>
   )
