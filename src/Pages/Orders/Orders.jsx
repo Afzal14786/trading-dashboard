e4abcbdac0286e6724ce8c88667fc91680e91e2b
Afzal14786 <mdafzal14777@gmail.com>
2025-08-16 02:04:46 +0530

    Implementing Order Page

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import "./style.css"
 
 const Orders = () => {
   return (
     <>
-      <div className='summary'>
-        <h1>I am order page</h1>
+      <div className='summary order__book'>
+        <div className="order__status">
+          <div className="desc">
+            <img src="/images/orderbook.svg" alt="order Book" className='order__book__img' />
+            <p className='desc__text'>You haven't placed any orders today</p>
+          </div>
+
+          <div className="btn">
+            <button className='get__started__btn'>Get Started</button>
+            <a href="#" className='view__history'>View History</a>
+          </div>
+        </div>
       </div>
     </>
   )
