099915c782e82e573b1ba09a850d3f753984ca01
Afzal14786 <mdafzal14777@gmail.com>
2025-08-21 10:16:57 +0530

    adding new profile page

---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+const Profile = ()=> {
+    return (
+      <>
+        <div className="summary profile_container">
+            <h1>Hi i am profile page</h1>
+        </div>
+      </>  
+    );
+}
+
+
+export default Profile
