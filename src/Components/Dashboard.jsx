b14f494f2963a9ebce7c21198de9f92ac80826f6
Afzal14786 <mdafzal14777@gmail.com>
2025-08-21 23:04:48 +0530

    fixing route

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Sidebar from './Sidebar'
 
 import DashboardSummary from "../Pages/Dashboard/DashboardSummary"
 import Orders from "../Pages/Orders/Orders"
@@ -13,42 +11,45 @@ import SubHoldingPage from "../Pages/Holdings/SubHoldingPage"
 import HoldingsLayout from '../Pages/Holdings/HoldingsLayout'
 import EquityPage from '../Pages/Holdings/EquityPage'
 
-
 import OrdersLayout from '../Pages/Orders/OrdersLayout'
 import BasketsPage from '../Pages/Orders/BasketsPage'
 import GttPage from '../Pages/Orders/GttPage'
 import AlertsPage from '../Pages/Orders/AlertsPage'
 import SipPage from '../Pages/Orders/SipPage'
 
+import MainLayout from './MainLayout'
+import PathNotFound from '../PathNotFound'
+
 const Dashboard = () => {
   return (
-    <>
-        <div>
-            <Sidebar/>
-            <div>
-                <Routes>
-                    <Route element={<DashboardSummary/>} path='/'/>
-                    <Route element={<OrdersLayout/>} path='/orders'>
-                        <Route index element={<Orders/>} path='/orders'/>
-                        <Route element={<GttPage/>} path='/orders/gtt'/>
-                        <Route element={<BasketsPage/>} path='/orders/baskets'/>
-                        <Route element={<SipPage/>} path='/orders/sip'/>
-                        <Route element={<AlertsPage/>} path='/orders/alerts'/>
-                    </Route>
+    <Routes>
+      {/* Routes WITH sidebar + navbar */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<DashboardSummary />} />
 
-                    <Route element={<HoldingsLayout/>} path='/holdings'>
-                      <Route index element={<HoldingAll />} path='/holdings'/>
-                      <Route element={<HoldingAll />} path="/holdings/all"/>
-                      <Route element={<EquityPage />} path="/holdings/equity"/>
-                      <Route element={<SubHoldingPage/>} path='/holdings/mutualfund'/>
-                    </Route>
-                    <Route element={<Positions/>} path='/positions' />
-                    <Route element={<Funds/>} path='/funds' />
-                    <Route element={<Profile/>} path='/profile' />
-                </Routes>
-            </div>
-        </div>
-    </>
+        <Route path="/orders" element={<OrdersLayout />}>
+          <Route index element={<Orders />} />
+          <Route path="gtt" element={<GttPage />} />
+          <Route path="baskets" element={<BasketsPage />} />
+          <Route path="sip" element={<SipPage />} />
+          <Route path="alerts" element={<AlertsPage />} />
+        </Route>
+
+        <Route path="/holdings" element={<HoldingsLayout />}>
+          <Route index element={<HoldingAll />} />
+          <Route path="all" element={<HoldingAll />} />
+          <Route path="equity" element={<EquityPage />} />
+          <Route path="mutualfund" element={<SubHoldingPage />} />
+        </Route>
+
+        <Route path="/positions" element={<Positions />} />
+        <Route path="/funds" element={<Funds />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+
+      {/* Catch-all route WITHOUT layout */}
+      <Route path="*" element={<PathNotFound />} />
+    </Routes>
   );
 }
 
