d38a842b44c9d8f30938034b9a4e3e6cc47c20e3
Afzal14786 <mdafzal14777@gmail.com>
2025-09-01 23:07:05 +0530

    fixing the login uui

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -7,81 +7,63 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [isFocused, setIsFocused] = useState(false);
+  const [isPhoneFocused, setIsPhoneFocused] = useState(false);
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+  const [phoneOrUserId, setPhoneOrUserId] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <>
-      <div className="login_container">
-        {/* header section */}
-        <div className="login_page">
-          <div className="header">
-            <div className="profile_image_login">
-              <img src="/images/profile_test.jpg" alt="" />
-            </div>
-            <p className="user_name">CKV524</p>
-            <Link className="change_user">Change user</Link>
-          </div>
-
-          {/* Password field with floating label and toggle */}
-          <div className="password_div">
-            <div
-              className={`input_wrapper ${isFocused || password ? "focused" : ""}`}
-            >
-              <label htmlFor="password">Password</label>
-              <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                className="password_input"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                placeholder=""
-              />
-              <span
-                className="toggle_password"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <VisibilityOffIcon/> : <VisibilityIcon/>}
-              </span>
-            </div>
-            <button className="login_btn">Login</button>
-          </div>
-
-          <Link className="forgot">Forgot user ID or password?</Link>
+    <div className="login_container_new">
+      <div className="login_page_new">
+        <div className="logo_container">
+          {/* Logo similar to the image */}
+          <img src="kite-logo.svg" alt="Kite Logo" className="kite_logo" />
         </div>
+        
+        <h2 className="login_title">Login to Kite</h2>
+
+        {/* Phone or User ID field */}
+        <div className="input_wrapper_new">
+          <label htmlFor="phone_userid" className={isPhoneFocused || phoneOrUserId ? "floated" : ""}>
+            Phone or User ID
+          </label>
+          <input
+            type="text"
+            id="phone_userid"
+            className="input_field"
+            value={phoneOrUserId}
+            onChange={(e) => setPhoneOrUserId(e.target.value)}
+            onFocus={() => setIsPhoneFocused(true)}
+            onBlur={() => setIsPhoneFocused(false)}
+          />
+        </div>
+
+        {/* Password field with floating label and toggle */}
+        <div className="input_wrapper_new">
+          <label htmlFor="password" className={isPasswordFocused || password ? "floated" : ""}>
+            Password
+          </label>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            className="input_field"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onFocus={() => setIsPasswordFocused(true)}
+            onBlur={() => setIsPasswordFocused(false)}
+          />
+          <span
+            className="toggle_password_new"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <VisibilityOffIcon/> : <VisibilityIcon/>}
+          </span>
+        </div>
+
+        <button className="login_btn_new">Login</button>
+        <Link className="forgot_link">Forgot user ID or password?</Link>
       </div>
-
-      {/* footer section */}
-      <div className="login_footer">
-        <div className="logos">
-          <img src="/images/playstore.svg" alt="play store" />
-          <img src="/images/app_store.svg" alt="apple logo" />
-        </div>
-
-        <div className="zerodha_logo">
-          <img src="/images/zerodha-logo.svg" alt="zerodha logo" />
-        </div>
-
-        <div className="dont_have_an_account">
-          <Link className="sign_up">Don't have an account? Signup now!</Link>
-        </div>
-
-        <div className="login_description">
-          <p>
-            Zerodha Broking Limited: Member of <Link>NSE</Link>, <Link>BSE</Link>{" "}
-            ‐ SEBI Reg. no. INZ000031633, <Link>CDSL</Link> ‐ <Link>SEBI</Link>{" "}
-            Reg. no. IN-DP-431-2019 | <Link>Smart Online Dispute Resolution</Link> |{" "}
-            <Link>SEBI SCORES</Link>
-          </p>
-        </div>
-
-        <div className="version">
-          <p>v3.0.0</p>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
